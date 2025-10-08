f2aa06497e1447fe6e9345d6cf5ee653ab1b18c5
Joseph Carpenter <carpenter.joseph1998@gmail.com>
2025-10-08 17:56:24 +0200

    simplify nesting a bit

--- lib/eccairs/occurrence.rb
+++ lib/eccairs/occurrence.rb
@@ -18,52 +18,46 @@ module Eccairs
 
       def to_xml(xml)
         xml.Occurrence(entityId: ENTITY_ID) do
-          occurrence_attributes, entity_attributes = partition_entities
+          # Group entities by their section module relative to Eccairs::Occurrence
+          grouped = @entities.group_by { |e| extract_section_module(e) }
 
-          build_attributes_section(xml, occurrence_attributes) if occurrence_attributes.any?
-          build_entities_section(xml, entity_attributes) if entity_attributes.any?
+          grouped.each do |section_module, section_entities|
+            build_section(xml, section_module, section_entities)
+          end
         end
       end
 
       private
 
-      def partition_entities
-        occurrence_attributes = []
-        entity_attributes = []
-
-        @entities.each do |entity|
-          if entity.class.name.include?('::Occurrence::Entities::')
-            entity_attributes << entity
-          else
-            occurrence_attributes << entity
-          end
-        end
-
-        [occurrence_attributes, entity_attributes]
-      end
-
-      def build_attributes_section(xml, entities)
-        xml.ATTRIBUTES do
-          entities.sort_by { |e| e.class.sequence }.each { |e| e.build_xml(xml) }
-        end
-      end
-
-      def build_entities_section(xml, entities)
-        xml.ENTITIES do
-          entities.group_by { |e| extract_parent_module(e) }.each do |parent_module, grouped_entities|
-            parent_module.build_entity_xml(xml, grouped_entities)
-          end
-        end
-      end
-
-      def extract_parent_module(entity)
-        # Extract parent entity module from class name
+      def extract_section_module(entity)
+        # Remove "Eccairs::Occurrence::" prefix and get the section module
+        # e.g., Eccairs::Occurrence::Attributes::DewPoint -> Attributes
         # e.g., Eccairs::Occurrence::Entities::AerodromeGeneral::Attributes::AerodromeLatitude
-        # -> Eccairs::Occurrence::Entities::AerodromeGeneral
-        parts = entity.class.name.split('::')
-        entities_index = parts.index('Entities')
-        parent_module_name = parts[0..entities_index + 1].join('::')
-        Object.const_get(parent_module_name)
+        #       -> Entities::AerodromeGeneral
+        relative_path = entity.class.name.sub('Eccairs::Occurrence::', '')
+        parts = relative_path.split('::')
+
+        # If there are more than 2 parts, it's nested (e.g., Entities::AerodromeGeneral::Attributes::...)
+        # Take first 2 parts. Otherwise, just take the first part (e.g., Attributes::DewPoint)
+        parts.length > 2 ? parts[0..1].join('::') : parts.first
+      end
+
+      def build_section(xml, section_path, entities)
+        # Section path is like "Attributes" or "Entities::AerodromeGeneral"
+        section_name = section_path.split('::').first.upcase.to_sym
+
+        xml.send(section_name) do
+          if section_name == :ENTITIES
+            # For nested entities, group by parent module and build entity XML
+            entities.group_by { |e| extract_section_module(e) }.each do |module_path, grouped_entities|
+              parent_module = Object.const_get("Eccairs::Occurrence::#{module_path}")
+              parent_module.build_entity_xml(xml, grouped_entities.sort_by { |e| e.class.sequence })
+            end
+          else
+            # For direct attributes, just build XML for each
+            entities.sort_by { |e| e.class.sequence }.each { |e| e.build_xml(xml) }
+          end
+        end
       end
     end
   end
