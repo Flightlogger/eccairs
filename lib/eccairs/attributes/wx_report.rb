f3e8b1f204c69f60a31738005954ae7628aa5b4e
yashan waruna <yashan@flightlogger.net>
2025-10-29 13:17:23 +0530

    remove exist build_xml methods for String Attributes and set it to new TextAttribute

--- lib/eccairs/attributes/wx_report.rb
+++ lib/eccairs/attributes/wx_report.rb
@@ -2,21 +2,11 @@
 
 module Eccairs
   module Attributes
-    class WxReport < Eccairs::Base::StringAttribute
+    class WxReport < Eccairs::Base::TextAttribute
       attribute_id "177"
       xml_tag "Wx_Report"
       sequence 0
-
-      # Override build_xml to use PlainText child element
-      def build_xml(xml)
-        attrs = {attributeId: self.class.attribute_id}
-        attrs[:Unit] = self.class.unit if self.class.unit
-
-        xml.public_send(self.class.xml_tag, attrs) do
-          xml.parent.namespace = xml.parent.namespace_definitions.find { |ns| ns.prefix == "dt" }
-          xml["dt"].PlainText(@value)
-        end
-      end
+      text_type true
     end
   end
 end
